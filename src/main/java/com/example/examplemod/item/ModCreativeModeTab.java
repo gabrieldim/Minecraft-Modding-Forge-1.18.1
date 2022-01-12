bd4119adbd687134040f15e50f337b9c979a0dce
gdimitrievski <gabriel.dimitrievski@keitaro.com>
2022-01-12 21:30:35 +0100

    added creative menu

--- src/main/java/com/example/examplemod/item/ModCreativeModeTab.java
+++ src/main/java/com/example/examplemod/item/ModCreativeModeTab.java
@@ -4,12 +4,14 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
+
     public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab("tutorialModTab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.TITANIUM_INGOT.get());
         }
     };
+
     public static final CreativeModeTab TUTORIAL_TAB2 = new CreativeModeTab("tutorialModTab2") {
         @Override
         public ItemStack makeIcon() {
